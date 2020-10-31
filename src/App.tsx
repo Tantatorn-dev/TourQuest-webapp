f3ad9c89051ab40afb44d96ae3eb2afb8bc48f63
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-11-01 02:26:34 +0700

    map implement but still look a bit weird & profile data already working

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,18 @@ import './styles/main.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PlaceDetail from './pages/PlaceDetail';
 import QuestMap from "./pages/QuestMap";
+import Profile from "./pages/Profile";
 import Appbar from './components/Appbar';
 import CheckinQR from './components/CheckinQR';
 import 'leaflet/dist/leaflet.css';
 import SuccessPage from './pages/SuccessPage';
-import Modal from "react-modal";
-import Hint from './components/Hint';
-import { fireEvent } from '@testing-library/react';
+import Modal from 'react-modal';
 import Redeem from './pages/Redeem';
+
 
 Modal.setAppElement('#root');
 
 function App() {
-	const firebase = useFirebase();
 	return (
 		<Router>
 			<div className="gradient-teal h-screen">
@@ -41,10 +40,10 @@ function App() {
 						</Route>
 						<Route path='/success' component={SuccessPage}></Route>
 						<Route path='/redeem' component={Redeem} />
+					<Route path='/profile'>
+						<Profile />
+					</Route>
 					</div>
-					{/* <Route path='/profile'>
-						<Profile db={firebase.firestore()} />
-					</Route> */}
 				</Switch>
 			</div>
 		</Router>
