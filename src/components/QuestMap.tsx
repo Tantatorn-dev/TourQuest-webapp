feb415e2473072cc58bd3cb8586cd2db695e43bb
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-10-31 21:36:32 +0700

    polishing leaflet

--- src/components/QuestMap.tsx
+++ src/components/QuestMap.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Map, TileLayer, Circle } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
-import Appbar from "./Appbar";
+import { Map, TileLayer, Circle, Popup, Marker } from 'react-leaflet';
+import '../styles/leaflet.css';
+import './QuestMap.css';
 
 export default function QuestMap() {
+	let position = {
+		lat: 13.7516,
+		lng: 100.4927,
+	};
 
-    let position = {
-        lat: 13.7516,
-        lng: 100.4927
-    }
+	let zoom = 20;
 
-    let zoom = 17
-
-    return (
-        <div>
-            <Map center={position} zoom={zoom}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Circle center={position} radius={200}>
-                </Circle>
-            </Map>
-        </div>
-    )
+	return (
+		<div>
+			<Map center={position} zoom={zoom}>
+				<TileLayer
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+				/>
+				<Circle center={position} radius={200}></Circle>
+				<Marker position={position}>
+					<Popup>
+						fyi Building <br /> Dreamer working here!
+					</Popup>
+				</Marker>
+			</Map>
+            <div className='bg-blue-600 text-white font-bold py-2 px-4 m-2 rounded hint-icon hover:'>Hint found!</div>
+		</div>
+	);
 }
