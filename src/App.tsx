f31832498804637d3efe6fb7b285dffafc7cb343
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-10-31 16:37:42 +0700

    soliding PlaceDetail.tsx & setting up firebase on App.tsx & remove package-lock.json from git

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import QuestMap from "./components/QuestMap";
-import PlaceDetail from "./pages/PlaceDetail";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import firebase from 'firebase';
+import QuestMap from './components/QuestMap';
+import PlaceDetail from './pages/PlaceDetail';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/map">Map</Link>
-            </li>
-            <li>
-              <Link to="/detail">Detail</Link>
-            </li>
-          </ul>
-        </nav>
+	useEffect(() => {
+		const firebaseConfig = {
+			apiKey: 'AIzaSyDT56Jk03YNj86fIjcxfWzymlgmiuMrpaE',
+			authDomain: 'tourquest-c3659.firebaseapp.com',
+			databaseURL: 'https://tourquest-c3659.firebaseio.com',
+			projectId: 'tourquest-c3659',
+			storageBucket: 'tourquest-c3659.appspot.com',
+			messagingSenderId: '134535806597',
+			appId: '1:134535806597:web:d9e8ee69acbd24615c73ec',
+			measurementId: 'G-8DQWL099DW',
+		};
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig);
+		firebase.analytics();
+	}, []);
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/map'>Map</Link>
+						</li>
+						<li>
+							<Link to='/detail'>Detail</Link>
+						</li>
+					</ul>
+				</nav>
 
-        <Switch>
-          <Route path="/map">
-            <QuestMap />
-          </Route>
-          <Route path="/detail">
-            <PlaceDetail />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+				<Switch>
+					<Route path='/map'>
+						<QuestMap />
+					</Route>
+					<Route path='/detail'>
+						<PlaceDetail id={'2P0oCX8wWZdG7ISokBrI'} name={''} image={''} detail={''} />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/pages/PlaceDetail.tsx
+++ src/pages/PlaceDetail.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+// import firebase from 'firebase';
 
-function PlaceDetail(props: any) {
+function PlaceDetail({
+	name,
+	detail,
+	image,
+	id,
+}: {
+	name: string;
+	detail: string;
+	image: string;
+	id: string;
+}) {
+	// const [data, setData]: [data: any, setData: any] = useState(null);
+	// useEffect(() => {
+	// 	const fetchPlace = async () => {
+	// 		const placeRef = firebase
+	// 			.firestore()
+	// 			.collection('places')
+	// 			.doc(id);
+	// 		const doc = await placeRef.get();
+	// 		setData(doc.data());
+	// 	};
+	// 	fetchPlace();
+	// }, []);
 	return (
 		<>
-			<h3>Wat Phra Keaw</h3>
-			<img
-				src={
-					'https://lp-cms-production.imgix.net/2019-06/f38562393be73d571e111dbd0b060d58-wat-phra-kaew.jpg'
-				}
-				width={300}
-			></img>
-			<p>
-				Interdum et malesuada fames ac ante ipsum primis in
-				faucibus. Fusce leo augue, imperdiet sit amet odio a,
-				imperdiet condimentum ante. Integer nec leo id nibh
-				elementum posuere. Morbi vel lacus orci. Sed eget augue
-				aliquet velit consectetur egestas id quis velit. Nulla
-				facilisi. Etiam ac nisl a ligula tempor porta.
-			</p>
+			<h3>{name}</h3>
+			<img src={image} width={300}></img>
+			<p>{detail}</p>
 			<button>
 				<Link to='/map'>Map</Link>
 			</button>
